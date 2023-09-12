fc729709cdbb25c6687216867a70b9b4f108c6fa
Panan Chaimongkol <nnuna2012@gmail.com>
2023-09-12 15:07:12 +0700

    feat:FT refactor

--- functional_tests.py
+++ functional_tests.py
@@ -17,6 +17,34 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):  
         self.browser.quit()
 
+    
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
+         # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly"
+        # (Edith is very methodical)
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
+        # Satisfied, she goes back to sleep
+
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):  
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
