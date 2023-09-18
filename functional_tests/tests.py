15e1b0cae4ebd262ef6e25e704d7f00cb4c88e6f
Panan Chaimongkol <nnuna2012@gmail.com>
2023-09-18 15:20:34 +0700

    feat:new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,9 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 
+
+
+MAX_WAIT = 5
 
 class NewVisitorTest(LiveServerTestCase):  
     def setUp(self):
@@ -14,13 +18,65 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser = webdriver.Firefox(options=options)
         # self.browser = webdriver.Firefox()
 
+
     def test_can_start_a_todo_list(self):
-            self.browser.get(self.live_server_url)
+        self.browser.get(self.live_server_url)
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+          # Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+
+          # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, "/lists/.+")
+     
+        self.browser.delete_all_cookies()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertNotIn("make a fly", page_text)
+
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Buy milk")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, "/lists/.+")
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element(By.TAG_NAME, "body").text
+        self.assertNotIn("Buy peacock feathers", page_text)
+        self.assertIn("Buy milk", page_text)
+
 
     def tearDown(self):  
         self.browser.quit()
 
     
+    def wait_for_row_in_list_table(self, row_text):
+         start_time = time.time()
+         while True:
+              try:
+                   table = self.browser.find_element(By.ID, "id_nothing")
+                   rows = table.find_elements(By.TAG_NAME, "tr")
+                   self.assertIn("foo", [row.text for row in rows])
+                   return
+              except (AssertionError, WebDriverException):
+                   if time.time() - start_time > MAX_WAIT:
+                        raise
+                   time.sleep(0.5)
+          
+
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
@@ -46,6 +102,8 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, she goes back to sleep
 
+
+    
 
     
 
--- lists/views.py
+++ lists/views.py
@@ -8,11 +8,11 @@ from lists.models import Item
 def home_page(request):
     if request.method == "POST":
         Item.objects.create(text=request.POST["item_text"])
-        return redirect("/")
+        return redirect("/lists/the-only-list-in-the-world/")
     
-    items = Item.objects.all()
-    return render(request, "home.html", {"items": items})
-
+    # items = Item.objects.all()
+    # return render(request, "home.html", {"items": items})
+    return render(request, "home.html")
 
     # return render(
     #     request,
@@ -25,3 +25,9 @@ def home_page(request):
    # if request.method == "POST":
    #    return HttpResponse("You submitted: " + request.POST["item_text"])
    # return render(request, "home.html")
+
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, "list.html", {"items": items})
+
