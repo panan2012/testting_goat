a8c8dd8127b7d34396d3b557cb34bc489ae93892
Panan Chaimongkol <nnuna2012@gmail.com>
2023-09-26 13:36:40 +0700

    feat:first steps of FT for layout + styling

--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect,render
-from lists.models import Item
+from lists.models import Item, List
 # from django.http import HttpResponse
 # Create your views here.
 # home_page = None
 
 def home_page(request):
-    if request.method == "POST":
-        Item.objects.create(text=request.POST["item_text"])
-        return redirect("/lists/the-only-list-in-the-world/")
+    # if request.method == "POST":
+    #     Item.objects.create(text=request.POST["item_text"])
+    #     return redirect("/lists/the-only-list-in-the-world/")
     
     # items = Item.objects.all()
     # return render(request, "home.html", {"items": items})
@@ -31,3 +31,8 @@ def view_list(request):
     items = Item.objects.all()
     return render(request, "list.html", {"items": items})
 
+
+def new_list(request):
+    nulist = List.objects.create()
+    Item.objects.create(text=request.POST["item_text"], list=nulist)
+    return redirect("/lists/the-only-list-in-the-world/")
